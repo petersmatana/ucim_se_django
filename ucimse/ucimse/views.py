2db6673fdb139c7cb1eafed54c14b0b70d18a6e9
Peter Smatana <petersmatana@gmail.com>
2017-02-17 13:06:19 +0100

    templejty

--- ucimse/ucimse/views.py
+++ ucimse/ucimse/views.py
@@ -1,8 +1,33 @@
 from django.http import HttpResponse
+from django.template import Template, Context
+
+
+def _home_page_template():
+    template_string = """tohle je <b>{{ name }}</b> templejta<br><br>
+a ted nejaky polozky:
+<ul>{% for item in items %}
+<li>{{ item }}</li>
+{% endfor %}</ul>
+"""
+    return template_string
 
 
 def home_page(request):
-    return HttpResponse('home page')
+    t = Template(_home_page_template())
+    context = {
+        'name': 'smontyho',
+        'items': [1, 2, 3, 4],
+    }
+    c = Context(context)
+    return HttpResponse(t.render(c))
+
 
 def hello(request):
-    return HttpResponse('Hello Smonty! from Django.')
+    t = Template(_home_page_template())
+    context = {
+        'name': 'Django Rainhard',
+        'items': ['e', 'h', 'g', 'd', 'a', 'e'],
+    }
+    c = Context(context)
+    return HttpResponse(t.render(c))
+
